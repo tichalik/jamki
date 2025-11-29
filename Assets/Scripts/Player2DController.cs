a107befddc23b0c1058b2db49d74915b8c848f49
Jan Supierz <jan.supierz02@gmail.com>
2025-11-29 15:45:42 +0100

    Added moving

---
+++ Assets/Scripts/Player2DController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player2DCircleController : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
